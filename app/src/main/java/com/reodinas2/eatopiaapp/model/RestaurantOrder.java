04c1f184a61a17785283bec42d78d3aac1358d84
reodinas <120348461+reodinas@users.noreply.github.com>
2023-03-28 18:12:15 +0900

    주문확인 페이지 구현중

---
+++ app/src/main/java/com/reodinas2/eatopiaapp/model/RestaurantOrder.java
@@ -0,0 +1,53 @@
+package com.reodinas2.eatopiaapp.model;
+
+import java.util.List;
+
+public class RestaurantOrder {
+    private int people;
+    private String reservTime;
+    private int type;
+    private List<Menu> menuInfo;
+
+
+    public RestaurantOrder() {
+    }
+
+    public RestaurantOrder(int people, String reservTime, int type, List<Menu> menuInfo) {
+        this.people = people;
+        this.reservTime = reservTime;
+        this.type = type;
+        this.menuInfo = menuInfo;
+    }
+
+    public int getPeople() {
+        return people;
+    }
+
+    public void setPeople(int people) {
+        this.people = people;
+    }
+
+    public String getReservTime() {
+        return reservTime;
+    }
+
+    public void setReservTime(String reservTime) {
+        this.reservTime = reservTime;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public List<Menu> getMenuInfo() {
+        return menuInfo;
+    }
+
+    public void setMenuInfo(List<Menu> menuInfo) {
+        this.menuInfo = menuInfo;
+    }
+}
---
+++ app/src/main/java/com/reodinas2/eatopiaapp/model/RestaurantOrderRes.java
@@ -0,0 +1,22 @@
+package com.reodinas2.eatopiaapp.model;
+
+public class RestaurantOrderRes {
+    private String result;
+    private int orderId;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+}
