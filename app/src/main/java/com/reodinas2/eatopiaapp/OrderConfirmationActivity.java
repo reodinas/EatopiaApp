b730b5de67274a6b5069665b9a8e02ccf55d56f5
reodinas <120348461+reodinas@users.noreply.github.com>
2023-03-28 23:56:50 +0900

    주문확인페이지 데이터세팅 완. 레이아웃 수정필요

--- app/src/main/java/com/reodinas2/eatopiaapp/OrderConfirmationActivity.java
+++ app/src/main/java/com/reodinas2/eatopiaapp/OrderConfirmationActivity.java
@@ -2,6 +2,8 @@ package com.reodinas2.eatopiaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -26,7 +28,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
+import java.util.concurrent.CountDownLatch;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -51,6 +58,14 @@ public class OrderConfirmationActivity extends AppCompatActivity {
     TextView txtTel;
     Button btnHome;
 
+    Restaurant restaurantInfo;
+    Order orderInfo;
+    List<Menu> menuInfo;
+    CountDownLatch countDownLatch = new CountDownLatch(2);
+    private ProgressDialog dialog;
+    SimpleDateFormat sf; // UTC 타임존을 위한 변수
+    SimpleDateFormat df; // Local 타임존을 위한 변수
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,6 +84,7 @@ public class OrderConfirmationActivity extends AppCompatActivity {
         txtTel = findViewById(R.id.txtTel);
         btnHome = findViewById(R.id.btnHome);
 
+        showProgress("데이터 로딩 중...");
 
         // 인텐트에서 restaurantId와 orderId를 추출
         Intent intent = getIntent();
@@ -77,6 +93,24 @@ public class OrderConfirmationActivity extends AppCompatActivity {
 
         getRestaurantDetails();
         getOrderDetails();
+
+        // countDownLatch를 사용하여 두 개의 네트워크 호출 메서드가 모두 완료될 때까지 기다린 후에 UI를 업데이트
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    countDownLatch.await();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            updateUI();
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 
 
     }
@@ -91,22 +125,22 @@ public class OrderConfirmationActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<RestaurantRes> call, Response<RestaurantRes> response) {
                 if (response.isSuccessful()) {
-                    Restaurant restaurantInfo = response.body().getRestaurantInfo();
+                    restaurantInfo = response.body().getRestaurantInfo();
 
                     // TODO: 응답에서 받은 식당 정보를 사용하여 UI를 업데이트하거나 필요한 작업을 수행합니다.
                     // 화면 세팅
-                    String imgUrl = restaurantInfo.getImgUrl();
-                    Glide.with(OrderConfirmationActivity.this)
-                            .load(imgUrl)
-                            .placeholder(R.drawable.baseline_image_24)
-                            .centerCrop()
-                            .error(R.drawable.baseline_image_24)
-                            .into(imgRestaurant);
-
-                    txtRestaurantName.setText(restaurantInfo.getName());
-                    String address = restaurantInfo.getLocCity()+" "+restaurantInfo.getLocDistrict()+" "+restaurantInfo.getLocDetail();
-                    txtAddress.setText(address);
-                    txtTel.setText(restaurantInfo.getTel());
+//                    String imgUrl = restaurantInfo.getImgUrl();
+//                    Glide.with(OrderConfirmationActivity.this)
+//                            .load(imgUrl)
+//                            .placeholder(R.drawable.baseline_image_24)
+//                            .centerCrop()
+//                            .error(R.drawable.baseline_image_24)
+//                            .into(imgRestaurant);
+//
+//                    txtRestaurantName.setText(restaurantInfo.getName());
+//                    String address = restaurantInfo.getLocCity()+" "+restaurantInfo.getLocDistrict()+" "+restaurantInfo.getLocDetail();
+//                    txtAddress.setText(address);
+//                    txtTel.setText(restaurantInfo.getTel());
 
 
                     
@@ -122,12 +156,16 @@ public class OrderConfirmationActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
+
+                countDownLatch.countDown();
             }
 
             @Override
             public void onFailure(Call<RestaurantRes> call, Throwable t) {
                 Toast.makeText(OrderConfirmationActivity.this, "식당 정보를 가져오는데 실패했습니다.", Toast.LENGTH_SHORT).show();
                 Log.e("LOGCAT", String.valueOf(t));
+
+                countDownLatch.countDown();
             }
         });
     }
@@ -146,20 +184,13 @@ public class OrderConfirmationActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<OrderRes> call, Response<OrderRes> response) {
                 if (response.isSuccessful()) {
-                    Order orderInfo = response.body().getOrderInfo();
-                    List<Menu> menuInfo = response.body().getMenuInfo();
+                    orderInfo = response.body().getOrderInfo();
+                    menuInfo = response.body().getMenuInfo();
 
                     // TODO: 응답에서 받은 주문 정보와 메뉴 정보를 사용하여 UI를 업데이트하거나 필요한 작업을 수행합니다.
                     // 화면 세팅
-                    txtOrderId.setText(String.valueOf(orderInfo.getId()));
-                    txtPeople.setText(String.valueOf(orderInfo.getPeople()));
-
-
-                    txtMenu.setText(menuInfo.get(0).getMenuName() + "외 "+ (menuInfo.size()-1) + "개");
-//                    int price = 0;
-//                    for (Menu menu : menuInfo){
-//                        price + menu.getPrice()
-//                    }
+//                    txtPeople.setText(""+orderInfo.getPeople());
+//                    txtMenu.setText(menuInfo.get(0).getMenuName()+"외 "+menuInfo.size()+" 개");
 
 
                 } else {
@@ -174,15 +205,116 @@ public class OrderConfirmationActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
+
+                countDownLatch.countDown();
             }
 
             @Override
             public void onFailure(Call<OrderRes> call, Throwable t) {
                 Toast.makeText(OrderConfirmationActivity.this, "주문 정보를 가져오는데 실패했습니다.", Toast.LENGTH_SHORT).show();
                 Log.e("LOGCAT", String.valueOf(t));
+
+                countDownLatch.countDown();
             }
         });
     }
 
+    @SuppressLint("SetTextI18n")
+    private void updateUI() {
+        if (restaurantInfo != null && orderInfo != null && menuInfo != null) {
+            // restaurantInfo
+            String imgUrl = restaurantInfo.getImgUrl();
+            Glide.with(OrderConfirmationActivity.this)
+                    .load(imgUrl)
+                    .placeholder(R.drawable.baseline_image_24)
+                    .centerCrop()
+                    .error(R.drawable.baseline_image_24)
+                    .into(imgRestaurant);
+
+            txtRestaurantName.setText("식당명: " + restaurantInfo.getName());
+            String address = restaurantInfo.getLocCity()+" "+restaurantInfo.getLocDistrict()+" "+restaurantInfo.getLocDetail();
+            txtAddress.setText("식당위치: " + address);
+            txtTel.setText(restaurantInfo.getTel());
+
+            // orderInfo
+            txtOrderId.setText("주문번호: "+orderInfo.getId());
+            txtPeople.setText(String.valueOf(orderInfo.getPeople()));
+
+            String priceText;
+            if (orderInfo.getPriceSum() == -1) {
+                priceText = "가격정보 없음";
+            }else {
+                priceText = String.valueOf(orderInfo.getPriceSum());
+            }
+            txtPrice.setText("가격: " + priceText);
+
+            String typeText;
+            if (orderInfo.getType() == 1){
+                typeText = "포장";
+            } else {
+                typeText = "매장";
+            }
+            txtType.setText("주문종류: " + typeText);
+
+            // UTC => Local Time
+            String reservTime = orderInfo.getReservTime();
+            String createdAt = orderInfo.getCreatedAt();
+            sf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            sf.setTimeZone(TimeZone.getTimeZone("UTC"));
+            df.setTimeZone(TimeZone.getDefault());
+            try {
+                Date dateReservTime = sf.parse(reservTime);
+                txtReservTime.setText(df.format(dateReservTime));
+
+                Date dateCreatedAt = sf.parse(createdAt);
+                txtCreatedAt.setText(df.format(dateCreatedAt));
+
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+
+            // menuInfo
+            StringBuilder menuText = new StringBuilder();
+            int menuCount = menuInfo.size();
+            int maxDisplayedMenus = Math.min(3, menuCount);
+
+            for (int i = 0; i < maxDisplayedMenus; i++) {
+                Menu menu = menuInfo.get(i);
+                menuText.append(menu.getMenuName()).append(" ").append(menu.getCount()).append("개");
+
+                if (i < maxDisplayedMenus - 1) {
+                    menuText.append(", ");
+                }
+            }
+
+            if (menuCount > 3) {
+                menuText.append(" 외 ").append(menuCount - 3).append("종");
+            }
+
+            txtMenu.setText(menuText.toString());
+
+
+        } else {
+            Toast.makeText(OrderConfirmationActivity.this, "식당 정보 또는 주문 정보를 가져오는데 실패했습니다.", Toast.LENGTH_SHORT).show();
+        }
+
+        dismissProgress();
+
+    }
+
+    // 네트워크 로직 처리시에 화면에 보여주는 함수
+    public void showProgress(String message){
+        dialog = new ProgressDialog(this);
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setMessage(message);
+        dialog.show();
+    }
+
+
+    // 로직처리가 끝나면 화면에서 사라지는 함수
+    public void dismissProgress(){
+        dialog.dismiss();
+    }
 
 }
